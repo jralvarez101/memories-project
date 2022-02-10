a20cbcde4c453382b9ef1b5d0e59f6f88c30c437
martin <alvarez.martinjr@gmail.com>
2022-02-09 16:37:51 -0800

    file structure setup decoupled

---
+++ server/index.js
@@ -0,0 +1,5 @@
+// we can use the import syntax by using the following script: "type": "module" in pack.json
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Mongoose } from 'mongoose';
+import cors from 'cors';
