8ac90559425a4b126a87952577d6284299284858
martin <alvarez.martinjr@gmail.com>
2022-02-11 13:42:58 -0800

    updated post and postItem components

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import PostItem from './postItem/PostItem';
+import { Grid, CircularProgress } from '@material-ui/core';
 import useStyles from './styles';
 // get the posts (getting the state from the reducer)
 import { useSelector } from 'react-redux';
 
 function Posts() {
   // with useSelector you have access to the whole state in all the reducers, in this case you only have posts
-  const posts = useSelector((state) => console.log(state.posts));
+  const posts = useSelector((state) => state.posts);
 
   const classes = useStyles();
-  return (
-    <>
-      Posts
-      <PostItem />
-      <PostItem />
-    </>
+  return !posts.lengt ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid item key={post._id} xs={12} sm={6}>
+          <PostItem post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
